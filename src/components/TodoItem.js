891b4a6dc3d91083c142a7c76485ea408a4abbbe
박소정 <thwjd0235@naver.com>
2024-10-21 21:54:32 +0900

    feat: 회원가입, 로그인 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = ({item, deleteTask, toggleComplete}) => {
-  console.log('todolist',item)
-  
+const TodoItem = ({ item, deleteTask, toggleComplete }) => {
+  console.log("todolist", item);
 
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item ${item.isComplete ? 'item-complete':''}`}>
+        <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
           <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete" onClick={()=>deleteTask(item._id)}>삭제</button>
-            <button className="button-delete" onClick={()=>toggleComplete(item._id)}>{item.isComplete?'안끝남':'끝남'}</button>
-                                          
+            <button
+              className="button-delete"
+              onClick={() => deleteTask(item._id)}
+            >
+              삭제
+            </button>
+            <button
+              className="button-delete"
+              onClick={() => toggleComplete(item._id)}
+            >
+              {item.isComplete ? "안끝남" : "끝남"}
+            </button>
           </div>
         </div>
       </Col>
